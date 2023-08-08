6efb6cb8625198804789cb6bbca740808971086f
Parassahu2308 <sahujuparas786@gmail.com>
2023-08-08 14:39:39 +0530

    some changes

--- task3/src/component/ApiData.js
+++ task3/src/component/ApiData.js
@@ -2,48 +2,37 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const ApiData = () => {
-  const [phone, setPhone] = useState();
-  const [headers, setHeaders] = useState("");
+  const [responseHeaders, setResponseHeaders] = useState({});
 
   const postData = async () => {
-    const data = {
-      phonenumber: phone,
-    };
+    const apiUrl = "https://chimpu.xyz/api/post.php";
+    const requestData = { phonenumber: "8933062308" };
 
     try {
-      const response = await fetch("https://chimpu.xyz/api/post.php", {
-        method: "POST",
-        body: data,
+      // const response = await axios.post(apiUrl, requestData, {
+      //   withCredentials: false,
+      // });
+      const response = await axios({
+        method: "post",
+        url: apiUrl,
+        withCredentials: false,
+        data: requestData,
       });
 
-      // const response = await axios.post(
-      //   "https://chimpu.xyz/api/post.php",
-      //   data
-      // );
-
-      const headersText = response.headers.toString();
-      console.log(headersText);
-      setHeaders(headersText);
+      const headers = response.headers;
+      console.log(headers);
+      setResponseHeaders(headers);
     } catch (error) {
-      console.log("Error:", error);
+      console.error("Error:", error);
     }
   };
-
   return (
     <div>
-      <h1>API Data Display</h1>
-      <input
-        type="tel"
-        placeholder="Enter Phone Number"
-        onChange={(e) => setPhone(e.target.value)}
-      />
-      <button onClick={postData}>Post Data and Display Headers</button>
-      {headers && (
-        <div>
-          <h2>Received Headers:</h2>
-          <pre>{headers}</pre>
-        </div>
-      )}
+      <button onClick={postData}>Send Data</button>
+      <div>
+        <h2>Response Headers:</h2>
+        <pre>{JSON.stringify(responseHeaders)}</pre>
+      </div>
     </div>
   );
 };
